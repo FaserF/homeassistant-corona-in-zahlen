0e46866ff9f09088fc9cb91daa8967a3d3f8a4a0
Fabian Seitz <seitzf1@yahoo.de>
2021-03-20 13:38:37 +0100

    Delete custom_components/corona_in_zahlen directory

--- custom_components/corona_in_zahlen/const.py
+++
@@ -1,5 +0,0 @@
-"""Constants for the corona_in_zahlen integration."""
-DOMAIN = "corona_in_zahlen"
-BASE_DOMAIN = "https://www.corona-in-zahlen.de/landkreise/"
-ATTRIBUTION = "Data provided by corona-in-zahlen.de"
-OPTION_TOTAL = "total"
